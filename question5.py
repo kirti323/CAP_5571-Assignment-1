1648301a612209f907a83d43dd4097114fb1ac94
kirti323 <56168636+kirti323@users.noreply.github.com>
2024-01-30 22:02:34 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -16,8 +16,8 @@ def question5_2():
 
 def question5_3():
     answer = {
-        'bin1': [1, 6, 7, 8, 9],
+        'bin1': [1, 5, 6, 7],
         'bin2': [2, 3, 4],
-        'bin3': [5]
+        'bin3': [8, 9]
     }
     return answer 
